51d6a114a9785aa01ee11bfb4af5e47e570e7978
Glaider <avHITlearn@gmail.com>
2015-10-26 12:07:12 +0200

    New Lesson

--- www/index.php
+++ www/index.php
@@ -1,1 +1,22 @@
 <?php
+
+class Article
+{
+    public $title;
+    public $text;
+
+    public function __construct($title, $text)
+    {
+        $this->title = $title;
+        $this->text = $text;
+    }
+
+    public function view()
+    {
+        echo $this->title;
+        echo "<br>";
+        echo $this->text;
+    }
+}
+$a = new Article('news Titile','news Text');
+$a->view();
